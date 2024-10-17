f6e738fc68fd34c3c3cebf53f6adedc83bd48107
learnwitheunjae <git@stackblitz.com>
2024-10-17 10:55:02 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="w-96 mx-auto my-12">
+      <h1 className="text-2xl">Vite + React!!</h1>
+      <div>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
